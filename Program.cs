966c39d43753c267925895d1b69d500c29324113
simaoazeitona <simaoazeitona3@gmail.com>
2024-04-23 10:09:00 -0800

    Versao Final

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira os dois numeros");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha uma opção: \n1- Adicionar \n2- Subtrair \n3- Multiplicar \n4- Dividir ");        
+        }
+    }
+}
+
+ 
